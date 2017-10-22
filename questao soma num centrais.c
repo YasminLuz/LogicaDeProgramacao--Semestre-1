d7dcb2e54aaf7aaba6fc0b3a538ff04241ffe05e
YasminLuz <31142485+YasminLuz@users.noreply.github.com>
2017-10-21 23:30:28 -0300

    Rename questao sala soma centrais.c to questao soma num centrais.c

--- questao soma num centrais.c
+++ questao soma num centrais.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
-Soma dos números centrais de um	número menor que 9999 e mostre a soma dos dígitos centrais(2ºe3º dígitos).Assim, se o numero for 4326, a soma deverá ser 5 (3+2)
+Soma dos nÃºmeros centrais deÂ um	nÃºmeroÂ menor que 9999Â eÂ mostreÂ a somaÂ dosÂ dÃ­gitos centrais(2Âºe3ÂºÂ dÃ­gitos).Assim, se o numero for 4326, a soma deverÃ¡ ser 5 (3+2)
 */
 
 
@@ -9,7 +9,7 @@ main(){
 	
 	int num, r1, r2, r3, q2, q3, soma;
 	
-	printf("Digite um número entre 0 - 9999: \n");
+	printf("Digite um nÃºmero entre 0 - 9999: \n");
 	scanf("%d", &num);
 	
 	if (num> 0 && num< 9999){
@@ -24,7 +24,7 @@ main(){
 		
 	    soma = q2+ q3;
 	    
-	printf("A soma é: %d", soma);
+	printf("A soma Ã©: %d", soma);
 	} else {
 		printf("Digite um numero dentro do intervalo");
 	}
